1ceae00e6ebd1af97540fa5b85f0b5281f28d796
Gaurang Sharma <64519654+GaurangSharma18@users.noreply.github.com>
2022-12-29 00:45:12 +0200

    Base Commit

---
+++ official/assets/js/custom.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+      
+
+
+
+
+      
+});
+
+
+$('.projectCarouselAll').carousel({
+  interval:2000
+})
+
+    $('.projectCarouselAll .carousel-item').each(function(){
+      var next = $(this).next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<2;i++) {
+        next=next.next();
+        if (!next.length) {
+          next = $(this).siblings(':first');
+        }
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+    });
